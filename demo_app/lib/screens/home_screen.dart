b25719cc14981f4b6ad5322be2dcb955d0df8e21
priyanshjain117 <jainpriyanshu117@gmail.com>
2025-06-10 16:38:16 +0530

    post req to backend applied

--- demo_app/lib/screens/home_screen.dart
+++ demo_app/lib/screens/home_screen.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 
+import 'package:demo_app/screens/list_user_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+    HomeScreen({super.key});
+    String name='';
+    String email='';
 
   Future<void> _connectingToBackend() async {
     const url = 'http://localhost:3000/';
@@ -15,17 +18,86 @@ class HomeScreen extends StatelessWidget {
     }
   }
 
+  Future<void> _postRequestCheck() async {
+    const url = 'http://localhost:3000/user';
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({'name': name, 'email': email}),
+    );
+
+      final Map<String, dynamic> jsonData = json.decode(response.body);
+      print(jsonData['msg']);
+      print(jsonData['user']['name']);
+    
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            print("hello from button!");
-            _connectingToBackend();
-          },
-          style: TextButton.styleFrom(backgroundColor: Colors.lightGreenAccent),
-          child: const Text("click to check backend!"),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.deepOrangeAccent,
+              Colors.redAccent,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextField(
+                    decoration: InputDecoration(
+                      labelText: 'name',
+                    ),
+                    onChanged: (value) {
+                      name = value;
+                    }),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'email',
+                  ),
+                  onChanged: (value) {
+                    email = value;
+                  },
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton(
+                      style: TextButton.styleFrom(backgroundColor: Colors.orange),
+                      onPressed: _postRequestCheck,
+                      child: const Text("submit"),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(backgroundColor: Colors.orange),
+                      onPressed: (){
+                        Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=>ListUserScreen(),
+                        ),);
+                      },
+                      child: const Text("List all users"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
---
+++ demo_app/lib/screens/list_user_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ListUserScreen extends StatelessWidget {
+  const ListUserScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+  
+}
