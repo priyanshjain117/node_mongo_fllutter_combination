6d8c735b454478f6a549d411eae0538771348a67
priyanshjain117 <jainpriyanshu117@gmail.com>
2025-06-10 13:46:09 +0530

    backend successdully connscted to app

--- demo_app/lib/screens/home_screen.dart
+++ demo_app/lib/screens/home_screen.dart
@@ -1,14 +1,30 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  Future<void> _connectingToBackend() async {
+    const url = 'http://localhost:3000/';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonData = json.decode(response.body);
+      print(jsonData["msg"]);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: TextButton(
-          onPressed: () {},
+          onPressed: () {
+            print("hello from button!");
+            _connectingToBackend();
+          },
+          style: TextButton.styleFrom(backgroundColor: Colors.lightGreenAccent),
           child: const Text("click to check backend!"),
         ),
       ),
